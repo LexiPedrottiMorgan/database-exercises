2407dea65d071dfc2b36cf3de0a853132b07fcba
Lexi Morgan <leximorgan816@gmail.com>
2019-01-30 11:56:52 -0600

    updated comment

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -39,7 +39,7 @@ GROUP BY gender
 ORDER BY COUNT(gender)DESC;
 
 
-# counts total of the names that are M and F and dsiplays count by name and gender:
+# counts total of the names that are M and F and displays count by name and gender:
 SELECT first_name, gender, COUNT(*)
 FROM EMPLOYEES
 WHERE first_name IN ('IRENA', 'VIDYA', 'MAYA')
