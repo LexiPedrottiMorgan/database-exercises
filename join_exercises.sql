5ee464de74e304b0828018d1ecb06172f0f69d1c
Lexi Morgan <leximorgan816@gmail.com>
2019-02-04 16:47:41 -0600

    added managers that are female table

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,10 +1,19 @@
 USE employees;
 
 
-
+-- query that shows each department along with the name of the current manager for that department
 SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ',employees.last_name) AS 'Department Manager'
 FROM employees
 JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_manager.dept_no
 WHERE dept_manager.to_date = '9999-01-01'
+;
+
+
+-- Find the name of all departments currently managed by women
+SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ',employees.last_name) AS 'Department Manager'
+FROM employees
+JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
+JOIN departments ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date = '9999-01-01' AND employees.gender = 'f'
 ;
