3b79b1551a60e4c2b6a2c0affe06728308d5f2d1
Lexi Morgan <leximorgan816@gmail.com>
2019-02-04 12:36:37 -0600

    working on current manager name and dept

--- join_exercises.sql
+++ join_exercises.sql
@@ -2,10 +2,9 @@ USE employees;
 
 -- NOT FINISHED
 
-SELECT d.dept_name AS 'Department Name', dm.dept_manager_ibfk_1 AS 'Department Manager'
-FROM employees as e
-JOIN dept_manager as dm
-  ON dm.emp_no = e.emp_no
-JOIN departments as d
-  ON d.dept_no = dm.dept_no
-WHERE dm.to_date = '9999-01-01';
+SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ',employees.last_name) AS 'Department Manager'
+-- dept_name AS 'Department Name', dept_manager AS 'Department Manager'
+FROM employees
+JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
+JOIN departments ON departments.dept_no = dept_manager.dept_no
+;
