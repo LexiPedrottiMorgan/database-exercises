181f71b6fa89b800e5421ed5b6928136ddc43609
Lexi Morgan <leximorgan816@gmail.com>
2019-02-06 11:12:23 -0600

    finished customer service titles count

--- join_exercises.sql
+++ join_exercises.sql
@@ -17,3 +17,14 @@ JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_manager.dept_no
 WHERE dept_manager.to_date = '9999-01-01' AND employees.gender = 'f'
 ;
+
+
+-- Find the current titles of employees currently working in the Customer Service department
+SELECT titles.title AS 'Title',  COUNT(*) AS 'Count'
+FROM employees
+JOIN titles ON titles.emp_no = employees.emp_no
+JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE titles.to_date = '9999-01-01' AND employees.departments.dept_name = 'Customer Service'
+GROUP BY titles.title
+;
