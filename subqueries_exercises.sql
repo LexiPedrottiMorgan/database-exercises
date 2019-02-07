da179e602d9fd7d60c70b4b0035e39ca1404b0f1
Lexi Morgan <leximorgan816@gmail.com>
2019-02-07 15:15:59 -0600

    subqueries done.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -6,25 +6,33 @@ FROM employees
 WHERE hire_date IN (
     SELECT hire_date
     FROM employees
-    WHERE hire_date = 1990-10-22
+    WHERE emp_no = 101010
 );
 
--- finds the person with the employee number 101010
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE emp_no = 101010;
 
 
 -- TODO 2. Find all the titles held by all employees with the first name Aamod.
 -- 314 total titles, 6 unique titles
 
-SELECT first_name, last_name, title
-FROM employees
+SELECT title
+FROM titles
 WHERE emp_no IN
 (SELECT emp_no
-FROM titles
+FROM employees
 WHERE first_name = 'Aamod');
-
+--
 
 -- TODO 3. Find all the current department managers that are female.
 
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN
+  (SELECT emp_no
+  FROM dept_manager
+  WHERE emp_no IN
+    (SELECT emp_no
+    FROM employees
+    WHERE gender = 'F'
+    )
+AND to_date > now()
+)
